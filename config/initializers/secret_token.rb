8c5022f326bc33c776e2d8355abb742b8187551d
Asem Land <asem@antri.(none)>
2011-08-13 21:29:40 +0700

    initial commit for cinde

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cinde::Application.config.secret_token = 'a4963d15c3b448f66386115fa4aa2a42af405f5659cd99793bde0a65ece7822c6e238377f62eff4bb7df80b20f7ce54f9a4322d5ccec13492f77bdbae65225e2'
