471e6538c97395a72bee6ca51f0a548583572852
eggya <eggyachiquita@yahoo.com>
2011-09-10 13:56:01 +0700

    added login error notification

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,4 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+  $('body').prepend("<div id='notification'></div>");
+  $('#errorExplanation').detach().appendTo("#notification").delay(2000).fadeOut('slow');
+});
