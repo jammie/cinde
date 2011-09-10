3ae05f52dce7367d034d99f33c56b0db58c45100
eggya <eggyachiquita@yahoo.com>
2011-09-10 14:14:25 +0700

    added condition for login notification jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
-  $('body').prepend("<div id='notification'></div>");
-  $('#errorExplanation').detach().appendTo("#notification").delay(2000).fadeOut('slow');
+  if($('#login_box').size() > 0){
+    $('body').prepend("<div id='notification'></div>");
+    $('#errorExplanation').detach().appendTo("#notification").delay(2000).fadeOut('slow');
+  }
 });
