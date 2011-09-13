636fba5ecf500991be66f9e5de1ce1641f2ca121
eggya <eggyachiquita@yahoo.com>
2011-09-13 21:39:14 +0700

    full ajax CRUD for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,41 @@
 class UsersController < ApplicationController
   before_filter :require_user
+  before_filter :load
   set_tab :user
 
-  def index
+  def load
     @users = User.all
+    @user = User.new
+  end
+
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "Successfully created User."
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "Successfully updated User."
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "Successfully destroyed User."
   end
 end
